3d9ce17f4db11393045b449345ecedbda78ba4c5
lili <379484437@qq.com>
2017-03-19 00:44:23 +0800

    MSRA multiScale, multiAngle

--- evaluatePoly_MSRATD500.m
+++ evaluatePoly_MSRATD500.m
@@ -1,6 +1,6 @@
 % evaluatePoly_shopSign
 clear all;
-
+DISPLAY = 0;
 %% dirs and files
 datasetName = 'MSRATD500';
 imgDataBase = '/home/lili/datasets/';
@@ -8,15 +8,15 @@ ssdDataBase = '/home/lili/codes/ssd/caffe-ssd/data';
 imgDir = fullfile(imgDataBase, datasetName, 'img', 'test');
 gtDir = fullfile(imgDataBase, datasetName, 'gt', 'test', 'txt', 'polyTextline');
 dtDir = fullfile(ssdDataBase, datasetName, 'test_poly');
-dtDir = '/home/lili/codes/testLinking/boxWord';
+%dtDir = '/home/lili/codes/testLinking/boxWord';
 %% process each image
 imgFiles = dir(fullfile(imgDir, '*.jpg'));
 nImg = length(imgFiles);
 %nImg = 50;
 for i = 1: nImg
-%     if i < 13
-%         continue;
-%     end
+    %     if i < 13
+    %         continue;
+    %     end
     imgRawName = imgFiles(i).name;
     gtFile = fullfile(gtDir, [imgRawName(1:end-3), 'txt']);
     dtFile = fullfile(dtDir, ['res_', imgRawName(1:end-3), 'txt']);
@@ -29,12 +29,13 @@ for i = 1: nImg
     [recall, precision, fscore, evalInfo(i)] = evalDetPoly(dtPoly, gtPoly);
     
     % show image and poly
-%     cla;
-    image = imread(fullfile(imgDir, imgRawName));
-    imshow(image);
-    displayPoly(gtPoly, 'r');
-    displayPoly(dtPoly, 'g');
-    
+    %     cla;
+    if DISPLAY
+        image = imread(fullfile(imgDir, imgRawName));
+        imshow(image);
+        displayPoly(gtPoly, 'r');
+        displayPoly(dtPoly, 'g');
+    end
     fprintf('%d:%s:\n', i, imgRawName);
     fprintf('recall = %.3f, precision = %.3f, f-score = %.3f\n', recall, precision, fscore);
     
--- evaluatePoly_testLinking.m
+++ evaluatePoly_testLinking.m
@@ -1,5 +1,6 @@
 % evaluatePoly_shopSign
 clear all;
+DISPLAY = 0;
 
 %% dirs and files
 datasetName = 'MSRATD500';
@@ -9,14 +10,16 @@ imgDir = fullfile(imgDataBase, datasetName, 'img', 'test');
 gtDir = fullfile(imgDataBase, datasetName, 'gt', 'test', 'txt', 'polyTextline');
 dtDir = fullfile(ssdDataBase, datasetName, 'test_poly');
 dtDir = '/home/lili/codes/testLinking/boxWord';
+logFile = fullfile(dtDir, '..', 'testLog.txt');
+fp = fopen(logFile, 'wt');
 %% process each image
 imgFiles = dir(fullfile(imgDir, '*.jpg'));
 nImg = length(imgFiles);
 %nImg = 50;
 for i = 1: nImg
-%     if i < 13
-%         continue;
-%     end
+    %     if i < 13
+    %         continue;
+    %     end
     imgRawName = imgFiles(i).name;
     gtFile = fullfile(gtDir, [imgRawName(1:end-3), 'txt']);
     dtFile = fullfile(dtDir, ['res_', imgRawName(1:end-3), 'txt']);
@@ -29,14 +32,18 @@ for i = 1: nImg
     [recall, precision, fscore, evalInfo(i)] = evalDetPoly(dtPoly, gtPoly);
     
     % show image and poly
-%     cla;
-    image = imread(fullfile(imgDir, imgRawName));
-    imshow(image);
-    displayPoly(gtPoly, 'r');
-    displayPoly(dtPoly, 'g');
-    
+    %     cla;
+    if DISPLAY
+        image = imread(fullfile(imgDir, imgRawName));
+        imshow(image);
+        displayPoly(gtPoly, 'r');
+        displayPoly(dtPoly, 'g');
+    end
     fprintf('%d:%s:\n', i, imgRawName);
     fprintf('recall = %.3f, precision = %.3f, f-score = %.3f\n', recall, precision, fscore);
+    
+    fprintf(fp, '%d:%s:\n', i, imgRawName);
+    fprintf(fp, 'recall = %.3f, precision = %.3f, f-score = %.3f\n', recall, precision, fscore);
     
 end
 
@@ -49,3 +56,5 @@ else
     fscore = 0;
 end
 fprintf('\nrecall = %.3f, precision = %.3f, fmeasure = %.3f\n', recall, precision, fscore);
+fprintf(fp, '\nrecall = %.3f, precision = %.3f, fmeasure = %.3f\n', recall, precision, fscore);
+fclose(fp);
